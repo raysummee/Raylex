f95f8a13d16c16d5ee2aa0bdcffd3e8c4196f219
Angshuman Barpujari <Angshuarin@gmail.com>
2020-07-21 17:03:59 +0530

    add a ugly like page

--- lib/ux/pages/likedPage.dart
+++ lib/ux/pages/likedPage.dart
@@ -1,10 +1,38 @@
+import 'package:Raylex/ux/components/lists/songList.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class LikedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar:AppBar(
+          title: Text(
+            "For your likes"
+          ),
+          backgroundColor: Colors.lightBlue.shade300,
+          bottom: TabBar(
+            isScrollable: false,
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(
+                text: "Songs",
+              ),
+              Tab(
+                text: "Playlists",
+              )
+            ]
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Text('data'),
+            Text("da")
+          ]
+        ),
+      ),
     );
   }
 }
